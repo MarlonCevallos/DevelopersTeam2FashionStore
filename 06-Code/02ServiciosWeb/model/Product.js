ae2b4e73a8c9bf0e4aa1da822d5b1d1a0838c250
Marlon Cevallos <mbcevallos3@espe.edu.ec>
2022-12-04 18:31:51 -0500

    Create model Product

--- 06-Code/02ServiciosWeb/model/Product.js
+++ 06-Code/02ServiciosWeb/model/Product.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose') //comunicación más facil con bdd
+const Schema = mongoose.Schema
 
+const productSchema = new Schema(
+    {
+        id: {type: Number},
+        name: {type: String},
+        description: {type: String},
+        quantity: {type: Number},
+        price: {type: Number},
+        profit: {type: Number}
+    }
+)
+
+module.exports = Product = mongoose.model('Product', productSchema)
