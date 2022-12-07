f41e4cad63952cbb6c563074ebe82489fde71893
Steven Santiago Jaramillo Ortega <66186319+StevenJaramillo96@users.noreply.github.com>
2022-12-07 18:22:41 -0500

    Create Cellar.js
    Actualizar Controller

---
+++ 06-Code/02ServiciosWeb/controller/Cellar.js
@@ -0,0 +1,112 @@
+const Cellar = require("../model/Cellar"); // we import our model to use it
+
+//POST
+const createCellar = (req, res) => {
+  const { id, name_product, quantity, entry_date, exit_date } = req.body;
+
+  const timeInCellar = "" + bussinessRuler(entry_date, exit_date) + " days";
+
+  const cellar = new Cellar({
+    id,
+    name_product,
+    quantity,
+    entry_date,
+    exit_date,
+    timeInCellar,
+  });
+
+  cellar.save((err, newCellar) => {
+    err && res.status(500).json(err.message);
+    res.status(200).json(newCellar);
+  });
+};
+
+//GET
+const getCellars = (req, res) => {
+  Cellar.find((err, cellars) => {
+    err && res.status(500).send(err.message);
+    res.status(200).json(cellars);
+  });
+};
+
+//GET by ID
+const getCellar = (req, res) => {
+  Cellar.find({ id: req.params.id }, (err, cellar) => {
+    err && res.status(500).send(err.message);
+    res.status(200).json(cellar);
+  });
+};
+
+//PUT
+const updateCellar = (req, res) => {
+  const { id, name_product, quantity, entry_date, exit_date } = req.body;
+  const timeInCellar = "" + bussinessRuler(entry_date, exit_date) + " days";
+
+  try {
+    Cellar.findOneAndUpdate(
+      { id: req.params.id },
+      {
+        id,
+        name_product,
+        quantity,
+        entry_date,
+        exit_date,
+        timeInCellar,
+      },
+      (err) => {
+        err && res.status(500).json(err.message);
+        res.status(200).json("http-code: 200, message: 'Cellar updated'");
+      }
+    );
+  } catch (error) {
+    res.status(404).json({ Error: "Cellar not found" });
+  }
+};
+
+//DELETE
+const deleteCellar = (req, res) => {
+  Product.findOneAndDelete({ id: req.params.id }, (err) => {
+    err && res.status(500).send(err.message);
+    res.status(200).json("http-code: 200, message: 'Cellar deleted'");
+  });
+};
+
+//BUSSINESS RULER
+const bussinessRuler = (entry_day, exit_day) => {
+  const _entry_day = new Date(
+    getYaer(entry_day),
+    getMonth(entry_day),
+    getDay(entry_day)
+  );
+  const _exit_day = new Date(
+    getYaer(exit_day),
+    getMonth(exit_day),
+    getDay(exit_day)
+  );
+
+  const diff = Math.abs(_entry_day - _exit_day);
+  const diffDays = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+  return diffDays;
+};
+
+const getYaer = (date) => {
+  return parseInt(date.substring(0, 4));
+};
+
+const getMonth = (date) => {
+  return parseInt(date.substring(5, 7));
+};
+
+const getDay = (date) => {
+  return parseInt(date.substring(8, 10));
+};
+
+//EXPORTS
+module.exports = {
+  createCellar,
+  getCellars,
+  getCellar,
+  updateCellar,
+  deleteCellar,
+};
