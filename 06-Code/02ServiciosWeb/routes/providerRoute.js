f514bcc93f0f81a13f9a278e8b0974bcc5e9471f
Michael Daniel Gudiño Guamani <79387010+MichaelG69@users.noreply.github.com>
2023-01-17 21:42:16 -0500

    changes in code

--- 06-Code/02ServiciosWeb/routes/providerRoute.js
+++ 06-Code/02ServiciosWeb/routes/providerRoute.js
@@ -1,10 +1,11 @@
-const ProviderController = require('../controller/Providers')
-const express = require('express')
-const router = express.Router()
+const ProviderController = require('../controller/Providers');
+const express = require('express');
+const router = express.Router();
 
-router.post('/addProvider',ProviderController.createProvider)
-router.get('/providers',ProviderController.getProviders)
-router.put('/updateProvider',ProviderController.updateProvider)
-router.delete('/deleteProvider',ProviderController.deleteProvider)
+router.get("/provider", ProviderController.getProviders);
+router.post("/provider", ProviderController.createProvider);
+router.get("/provider/:id", ProviderController.getProvider);
+router.put("/provider/:id", ProviderController.updateProvider);
+router.delete("/provider/:id", ProviderController.deleteProvider);
 
-module.exports = router
+module.exports = router;
