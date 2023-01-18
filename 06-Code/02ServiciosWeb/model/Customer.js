114042ac54eb7d6f6dfa50a5f843ce5880bf04ac
Carla Abigail Iza Caillatasig <95055559+carlaabigailizacaillatasig@users.noreply.github.com>
2023-01-17 22:46:57 -0500

    actualizacion

--- 06-Code/02ServiciosWeb/model/Customer.js
+++ 06-Code/02ServiciosWeb/model/Customer.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const CustomersSchema = new Schema(
-    {
-        id: { type: String },
-        name: { type: String },
-        lastname: { type: String },
-        email: { type: String },
-        phone: { type: Number }
-    }
-)
+  {
+    id: { type: Number },
+    name: { type: String },
+    lastname: { type: String },
+    email: { type: String },
+    phone: { type: Number },
+  },
+  {
+    collection: "customers",
+    timestamps: false,
+    versionKey: false,
+  }
+);
 
-module.exports = Customer = mongoose.model('Customer', CustomersSchema)
+module.exports = Customer = mongoose.model("Customer", CustomersSchema);
