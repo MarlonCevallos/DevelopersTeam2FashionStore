f3f8ae8cf173720b7bff9f8425ecac29e4e1581a
Marlon Cevallos <mbcevallos3@espe.edu.ec>
2023-01-18 09:37:51 -0500

    Create component products

---
+++ 06-Code/03WebClient/src/components/Products/ProductItem.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import { deleteProduct } from "../../services/axiosProducts";
+
+const ProductItem = ({ product, getProducts }) => {
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    if (!window.confirm("¿Estás seguro de eliminar este producto?")) return;
+
+    await deleteProduct(product.id);
+    getProducts();
+  };
+
+  const handleUpdate = () => {
+    localStorage.setItem("product", JSON.stringify(product));
+    navigate("/products/add");
+  };
+
+  return (
+    <tr>
+      <td className="text-center">{product.name}</td>
+      <td className="text-center">{product.description}</td>
+      <td className="text-center">{product.price}</td>
+      <td className="text-center">{product.quantity}</td>
+      <td className="text-center">{product.total}</td>
+      <td className="text-center">
+        <button className="btn btn-primary me-2" onClick={handleUpdate}>
+          Editar
+        </button>
+        <button className="btn btn-danger" onClick={handleDelete}>
+          Eliminar
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default ProductItem;
