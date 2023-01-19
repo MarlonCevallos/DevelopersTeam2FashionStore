294311ec096b2e15514762dbb6114bf9af525822
Carla Abigail Iza Caillatasig <95055559+carlaabigailizacaillatasig@users.noreply.github.com>
2023-01-18 21:37:15 -0500

    version

---
+++ 06-Code/03WebClient/src/services/axiosCustomers.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+const URI = "http://localhost:3002/fashionStore/customer";
+
+export const getCustomers = async () => {
+  try {
+    const response = await axios.get(URI);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteCustomer = async (id) => {
+  try {
+    const response = await axios.delete(`${URI}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addCustomer = async (customer) => {
+  try {
+    const response = await axios.post(URI, customer);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateCustomer = async (customer) => {
+  try {
+    const response = await axios.put(`${URI}/${customer.id}`, customer);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
