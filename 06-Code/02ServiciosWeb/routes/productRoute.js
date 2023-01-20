bb3cab711dbb1c6755d8fd387832467a2b3f4bc9
Marlon Cevallos <mbcevallos3@espe.edu.ec>
2023-01-19 20:24:20 -0500

    Clean code

--- 06-Code/02ServiciosWeb/routes/productRoute.js
+++ 06-Code/02ServiciosWeb/routes/productRoute.js
@@ -2,15 +2,10 @@ const ProductsController = require('../controller/Products')
 const express = require('express')
 const router = express.Router()
 
-//router.post('/add', ProductsController.createProduct)
 router.post("/product", ProductsController.createProduct);
-//router.get('/products', ProductsController.getProducts)
 router.get("/product", ProductsController.getProducts);
-//router.get('/products/:id', ProductsController.getProduct)
 router.get("/product/:id", ProductsController.getProduct);
-//router.put('/updateProduct', ProductsController.updateProfit)
 router.put("/product/:id", ProductsController.updateProfit);
-//router.delete('/deleteProduct', ProductsController.deleteProduct)
 router.delete("/product/:id", ProductsController.deleteProduct);
 
 module.exports = router
