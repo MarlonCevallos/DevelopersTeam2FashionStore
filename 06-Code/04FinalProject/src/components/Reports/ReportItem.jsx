36306dabc66bc591f904db88acc43b0bb2c5235b
Carla Abigail Iza Caillatasig <95055559+carlaabigailizacaillatasig@users.noreply.github.com>
2023-02-08 17:23:26 -0500

    user requirements

---
+++ 06-Code/04FinalProject/src/components/Reports/ReportItem.jsx
@@ -0,0 +1,16 @@
+/* Author: Marlon Cevallos*/
+
+const ProductItem = ({ product, getProducts }) => {
+
+  return (
+    <tr>
+      <td className="text-center">{product.name}</td>
+      <td className="text-center">{product.description}</td>
+      <td className="text-center">{product.price}</td>
+      <td className="text-center">{product.quantity}</td>
+      <td className="text-center">{product.total}</td>
+    </tr>
+  );
+};
+
+export default ProductItem;
---
+++ 06-Code/04FinalProject/src/pages/Reports.jsx
@@ -0,0 +1,52 @@
+/* Author: Marlon Cevallos*/
+
+import ProductItem from "../components/Reports/ReportItem";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getProducts } from "../services/axiosProducts";
+
+const ListProducts = () => {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  const _getProducts = async () => {
+    const _products = await getProducts();
+    setProducts(_products);
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem("user")) navigate("/login");
+
+    _getProducts();
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center mt-4">Reporte ventas</h1>
+
+      <table className="table table-info mt-4">
+        <thead>
+          <tr>
+            <th className="text-center">NOMBRE</th>
+            <th className="text-center">DESCRIPCIÓN</th>
+            <th className="text-center">PRECIO</th>
+            <th className="text-center">CANTIDAD</th>
+            <th className="text-center">GANANCIAS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              getProducts={_getProducts}
+            />
+          ))}
+        </tbody>
+      </table>
+      {}
+    </div>
+  );
+};
+
+export default ListProducts;
